7a409033626080a148201627fbca7e6c52a1131d
Kousik Satish <kousiksatish@gmail.com>
2020-06-26 16:30:33 +0530

    Add message send and receive capabilities

--- index.js
+++ index.js
@@ -17,15 +17,15 @@ const server = net.createServer((socket) => {
             connectionArray.push(connectionData);
             console.log(`Connection created for ${connectionData.name}`);
             entrySucceeded = true;
+            socket.on('data', (data) => {
+                console.log(`Received data ${data.toString()}`);
+                processMessage(data.toString());
+            });
         } else {
             console.log(`Name already exists. Closing connection`);
             socket.end();
         }
         
-    });
-    
-    socket.on('data', (data) => {
-        console.log(`Received data ${data.toString()}`);
     });
 
     socket.on('end', () => {
@@ -61,6 +61,23 @@ function removeConnectionEntry(name) {
     } else {
         console.log(`Unable to remove connection entry of ${name}`);
     }
-    
+}
 
+function processMessage(msgString) {
+    const msg = JSON.parse(msgString)
+    if (msg.target !== undefined && msg.target.length !== 0) {
+        sendToOneUser(msg.target, msgString);
+    } else {
+        for(let i=0; i<connectionArray.length; i++) {
+            connectionArray[i].socket.write(msgString);
+        }
+    }
+}
+
+function sendToOneUser(name, msgString) {
+    for(let i=0; i<connectionArray.length; i++) {
+        if (connectionArray[i].name === name) {
+            connectionArray[i].socket.write(msgString);
+        }
+    }
 }
